6bfffd0a64a196bbb60fb1be4945b05968645547
ssaberan <ssaberan96@gmail.com>
2021-01-11 21:07:24 -0500

    implement recursive search in bestMove

--- src/ChessAI/BestMove.js
+++ src/ChessAI/BestMove.js
@@ -4,30 +4,41 @@ const bestMove = (chess, depth) => {
   const moves = chess.moves().sort(() => Math.random() - 0.5);
   const color = chess.fen().split(' ')[1];
 
-  let bestMove = '';
+  let bestAct = '';
   let bestEvaluation = color === 'w' ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
   let evaluation = 0;
+  let _ = {};
 
   for (const move of moves) {
-    chess.move(move);
-    evaluation = evaluate(chess.board());
-    chess.undo();
+    if (depth === 1) {
+      chess.move(move);
+      evaluation = evaluate(chess.board());
+      chess.undo();
+    } else if (depth > 1) {
+      chess.move(move);
+      // eslint-disable-next-line no-unused-vars
+      [_, evaluation] = bestMove(chess, depth - 1);
+      chess.undo();
+    } else {
+      console.error('Invalid depth');
+    }
+
     if (color === 'w') {
       if (evaluation > bestEvaluation) {
         bestEvaluation = evaluation;
-        bestMove = move;
+        bestAct = move;
       }
     } else if (color === 'b') {
       if (evaluation < bestEvaluation) {
         bestEvaluation = evaluation;
-        bestMove = move;
+        bestAct = move;
       }
     } else {
       console.error('Invalid color');
     }
   }
 
-  return bestMove;
+  return [bestAct, bestEvaluation];
 };
 
 export default bestMove;
